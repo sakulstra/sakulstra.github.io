78de5528a61ec377dae9b18c895676d5993d6a12
sakulstra <lukasstrassel@googlemail.com>
2015-04-24 20:00:05 +0200

    search fix

--- _static/doctools.js
+++ _static/doctools.js
@@ -1,1 +1,247 @@
-$u=_.noConflict();jQuery.urldecode=function(a){return decodeURIComponent(a).replace(/\+/g,' ');};jQuery.urlencode=encodeURIComponent;jQuery.getQueryParameters=function(a){if(typeof a=='undefined')a=document.location.search;var b=a.substr(a.indexOf('?')+1).split('&');var c={};for(var d=0;d<b.length;d++){var e=b[d].split('=',2);var f=jQuery.urldecode(e[0]);var g=jQuery.urldecode(e[1]);if(f in c)c[f].push(g);else c[f]=[g];}return c;};jQuery.contains=function(a,b){for(var c=0;c<a.length;c++)if(a[c]==b)return true;return false;};jQuery.fn.highlightText=function(a,b){function c(d){if(d.nodeType==3){var e=d.nodeValue;var f=e.toLowerCase().indexOf(a);if(f>=0&&!jQuery(d.parentNode).hasClass(b)){var g=document.createElement("span");g.className=b;g.appendChild(document.createTextNode(e.substr(f,a.length)));d.parentNode.insertBefore(g,d.parentNode.insertBefore(document.createTextNode(e.substr(f+a.length)),d.nextSibling));d.nodeValue=e.substr(0,f);}}else if(!jQuery(d).is("button, select, textarea"))jQuery.each(d.childNodes,function(){c(this);});}return this.each(function(){c(this);});};var a={init:function(){this.fixFirefoxAnchorBug();this.highlightSearchWords();this.initIndexTable();},TRANSLATIONS:{},PLURAL_EXPR:function(a){return a==1?0:1;},LOCALE:'unknown',gettext:function(b){var c=a.TRANSLATIONS[b];if(typeof c=='undefined')return b;return(typeof c=='string')?c:c[0];},ngettext:function(b,c,d){var e=a.TRANSLATIONS[b];if(typeof e=='undefined')return(d==1)?b:c;return e[a.PLURALEXPR(d)];},addTranslations:function(a){for(var b in a.messages)this.TRANSLATIONS[b]=a.messages[b];this.PLURAL_EXPR=new Function('n','return +('+a.plural_expr+')');this.LOCALE=a.locale;},addContextElements:function(){$('div[id] > :header:first').each(function(){$('<a class="headerlink">\u00B6</a>').attr('href','#'+this.id).attr('title',_('Permalink to this headline')).appendTo(this);});$('dt[id]').each(function(){$('<a class="headerlink">\u00B6</a>').attr('href','#'+this.id).attr('title',_('Permalink to this definition')).appendTo(this);});},fixFirefoxAnchorBug:function(){if(document.location.hash&&$.browser.mozilla)window.setTimeout(function(){document.location.href+='';},10);},highlightSearchWords:function(){var a=$.getQueryParameters();var b=(a.highlight)?a.highlight[0].split(/\s+/):[];if(b.length){var c=$('div.body');window.setTimeout(function(){$.each(b,function(){c.highlightText(this.toLowerCase(),'highlighted');});},10);$('<p class="highlight-link"><a href="javascript:Documentation.'+'hideSearchWords()">'+_('Hide Search Matches')+'</a></p>').appendTo($('#searchbox'));}},initIndexTable:function(){var a=$('img.toggler').click(function(){var a=$(this).attr('src');var b=$(this).attr('id').substr(7);$('tr.cg-'+b).toggle();if(a.substr(-9)=='minus.png')$(this).attr('src',a.substr(0,a.length-9)+'plus.png');else $(this).attr('src',a.substr(0,a.length-8)+'minus.png');}).css('display','');if(DOCUMENTATION_OPTIONS.COLLAPSE_INDEX)a.click();},hideSearchWords:function(){$('#searchbox .highlight-link').fadeOut(300);$('span.highlighted').removeClass('highlighted');},makeURL:function(a){return DOCUMENTATION_OPTIONS.URL_ROOT+'/'+a;},getCurrentURL:function(){var a=document.location.pathname;var b=a.split(/\//);$.each(DOCUMENTATION_OPTIONS.URL_ROOT.split(/\//),function(){if(this=='..')b.pop();});var c=b.join('/');return a.substring(c.lastIndexOf('/')+1,a.length-1);}};_=a.gettext;$(document).ready(function(){a.init();});
+/*
+ * doctools.js
+ * ~~~~~~~~~~~
+ *
+ * Sphinx JavaScript utilities for all documentation.
+ *
+ * :copyright: Copyright 2007-2011 by the Sphinx team, see AUTHORS.
+ * :license: BSD, see LICENSE for details.
+ *
+ */
+
+/**
+ * select a different prefix for underscore
+ */
+$u = _.noConflict();
+
+/**
+ * make the code below compatible with browsers without
+ * an installed firebug like debugger
+if (!window.console || !console.firebug) {
+  var names = ["log", "debug", "info", "warn", "error", "assert", "dir",
+    "dirxml", "group", "groupEnd", "time", "timeEnd", "count", "trace",
+    "profile", "profileEnd"];
+  window.console = {};
+  for (var i = 0; i < names.length; ++i)
+    window.console[names[i]] = function() {};
+}
+ */
+
+/**
+ * small helper function to urldecode strings
+ */
+jQuery.urldecode = function(x) {
+  return decodeURIComponent(x).replace(/\+/g, ' ');
+}
+
+/**
+ * small helper function to urlencode strings
+ */
+jQuery.urlencode = encodeURIComponent;
+
+/**
+ * This function returns the parsed url parameters of the
+ * current request. Multiple values per key are supported,
+ * it will always return arrays of strings for the value parts.
+ */
+jQuery.getQueryParameters = function(s) {
+  if (typeof s == 'undefined')
+    s = document.location.search;
+  var parts = s.substr(s.indexOf('?') + 1).split('&');
+  var result = {};
+  for (var i = 0; i < parts.length; i++) {
+    var tmp = parts[i].split('=', 2);
+    var key = jQuery.urldecode(tmp[0]);
+    var value = jQuery.urldecode(tmp[1]);
+    if (key in result)
+      result[key].push(value);
+    else
+      result[key] = [value];
+  }
+  return result;
+};
+
+/**
+ * small function to check if an array contains
+ * a given item.
+ */
+jQuery.contains = function(arr, item) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] == item)
+      return true;
+  }
+  return false;
+};
+
+/**
+ * highlight a given string on a jquery object by wrapping it in
+ * span elements with the given class name.
+ */
+jQuery.fn.highlightText = function(text, className) {
+  function highlight(node) {
+    if (node.nodeType == 3) {
+      var val = node.nodeValue;
+      var pos = val.toLowerCase().indexOf(text);
+      if (pos >= 0 && !jQuery(node.parentNode).hasClass(className)) {
+        var span = document.createElement("span");
+        span.className = className;
+        span.appendChild(document.createTextNode(val.substr(pos, text.length)));
+        node.parentNode.insertBefore(span, node.parentNode.insertBefore(
+          document.createTextNode(val.substr(pos + text.length)),
+          node.nextSibling));
+        node.nodeValue = val.substr(0, pos);
+      }
+    }
+    else if (!jQuery(node).is("button, select, textarea")) {
+      jQuery.each(node.childNodes, function() {
+        highlight(this);
+      });
+    }
+  }
+  return this.each(function() {
+    highlight(this);
+  });
+};
+
+/**
+ * Small JavaScript module for the documentation.
+ */
+var Documentation = {
+
+  init : function() {
+    this.fixFirefoxAnchorBug();
+    this.highlightSearchWords();
+    this.initIndexTable();
+  },
+
+  /**
+   * i18n support
+   */
+  TRANSLATIONS : {},
+  PLURAL_EXPR : function(n) { return n == 1 ? 0 : 1; },
+  LOCALE : 'unknown',
+
+  // gettext and ngettext don't access this so that the functions
+  // can safely bound to a different name (_ = Documentation.gettext)
+  gettext : function(string) {
+    var translated = Documentation.TRANSLATIONS[string];
+    if (typeof translated == 'undefined')
+      return string;
+    return (typeof translated == 'string') ? translated : translated[0];
+  },
+
+  ngettext : function(singular, plural, n) {
+    var translated = Documentation.TRANSLATIONS[singular];
+    if (typeof translated == 'undefined')
+      return (n == 1) ? singular : plural;
+    return translated[Documentation.PLURALEXPR(n)];
+  },
+
+  addTranslations : function(catalog) {
+    for (var key in catalog.messages)
+      this.TRANSLATIONS[key] = catalog.messages[key];
+    this.PLURAL_EXPR = new Function('n', 'return +(' + catalog.plural_expr + ')');
+    this.LOCALE = catalog.locale;
+  },
+
+  /**
+   * add context elements like header anchor links
+   */
+  addContextElements : function() {
+    $('div[id] > :header:first').each(function() {
+      $('<a class="headerlink">\u00B6</a>').
+      attr('href', '#' + this.id).
+      attr('title', _('Permalink to this headline')).
+      appendTo(this);
+    });
+    $('dt[id]').each(function() {
+      $('<a class="headerlink">\u00B6</a>').
+      attr('href', '#' + this.id).
+      attr('title', _('Permalink to this definition')).
+      appendTo(this);
+    });
+  },
+
+  /**
+   * workaround a firefox stupidity
+   */
+  fixFirefoxAnchorBug : function() {
+    if (document.location.hash && $.browser.mozilla)
+      window.setTimeout(function() {
+        document.location.href += '';
+      }, 10);
+  },
+
+  /**
+   * highlight the search words provided in the url in the text
+   */
+  highlightSearchWords : function() {
+    var params = $.getQueryParameters();
+    var terms = (params.highlight) ? params.highlight[0].split(/\s+/) : [];
+    if (terms.length) {
+      var body = $('div.body');
+      window.setTimeout(function() {
+        $.each(terms, function() {
+          body.highlightText(this.toLowerCase(), 'highlighted');
+        });
+      }, 10);
+      $('<p class="highlight-link"><a href="javascript:Documentation.' +
+        'hideSearchWords()">' + _('Hide Search Matches') + '</a></p>')
+          .appendTo($('#searchbox'));
+    }
+  },
+
+  /**
+   * init the domain index toggle buttons
+   */
+  initIndexTable : function() {
+    var togglers = $('img.toggler').click(function() {
+      var src = $(this).attr('src');
+      var idnum = $(this).attr('id').substr(7);
+      $('tr.cg-' + idnum).toggle();
+      if (src.substr(-9) == 'minus.png')
+        $(this).attr('src', src.substr(0, src.length-9) + 'plus.png');
+      else
+        $(this).attr('src', src.substr(0, src.length-8) + 'minus.png');
+    }).css('display', '');
+    if (DOCUMENTATION_OPTIONS.COLLAPSE_INDEX) {
+        togglers.click();
+    }
+  },
+
+  /**
+   * helper function to hide the search marks again
+   */
+  hideSearchWords : function() {
+    $('#searchbox .highlight-link').fadeOut(300);
+    $('span.highlighted').removeClass('highlighted');
+  },
+
+  /**
+   * make the url absolute
+   */
+  makeURL : function(relativeURL) {
+    return DOCUMENTATION_OPTIONS.URL_ROOT + '/' + relativeURL;
+  },
+
+  /**
+   * get the current relative url
+   */
+  getCurrentURL : function() {
+    var path = document.location.pathname;
+    var parts = path.split(/\//);
+    $.each(DOCUMENTATION_OPTIONS.URL_ROOT.split(/\//), function() {
+      if (this == '..')
+        parts.pop();
+    });
+    var url = parts.join('/');
+    return path.substring(url.lastIndexOf('/') + 1, path.length - 1);
+  }
+};
+
+// quick alias for translations
+_ = Documentation.gettext;
+
+$(document).ready(function() {
+  Documentation.init();
+});
